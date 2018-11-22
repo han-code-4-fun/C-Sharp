bbff7fdbd286fa263742060c38112c3768a92c8a
miaonagemide <hanzhou244@gmail.com>
2018-11-22 15:47:11 -0800

    new

---
+++ PracticeCode/EntityFramework/DemoConsole/DemoConsole/EF/ActorDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoConsole.EF
+{
+    class ActorDbContext : DbContext
+    {
+
+        public DbSet<Actor> Actors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ActorDb;"
+                +"Trusted_connection=True;");
+        }
+    }
+}
