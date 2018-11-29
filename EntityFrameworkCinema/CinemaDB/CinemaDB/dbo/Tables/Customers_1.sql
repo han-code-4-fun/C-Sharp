9445e9ea47d2fd38746833d839d7f42b3d941ee3
hoang <hoang.mai@smartt.com>
2018-11-28 21:29:34 -0800

    cinema db

---
+++ EntityFrameworkCinema/CinemaDB/CinemaDB/dbo/Tables/Customers_1.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [CustomerId] INT           NOT NULL,
+    [FirstName]  VARCHAR (MAX) NULL,
+    [Phone]      VARCHAR (MAX) NOT NULL,
+    [Email]      VARCHAR (MAX) NOT NULL,
+    [LastName]   VARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC)
+);
+
---
+++ EntityFrameworkCinema/CinemaDB/CinemaDB/dbo/Tables/Orders_1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Orders] (
+    [OrderId]          INT            NOT NULL,
+    [CustomerId]       INT            NOT NULL,
+    [ScheduleId]       INT            NOT NULL,
+    [OrderTotal]       MONEY          NOT NULL,
+    [OrderDate]        ROWVERSION     NOT NULL,
+    [TicketIdentifier] NVARCHAR (MAX) NOT NULL,
+    [NumberOfTickets]  INT            NOT NULL,
+    [Seats]            VARCHAR (MAX)  NULL,
+    PRIMARY KEY CLUSTERED ([OrderId] ASC),
+    CONSTRAINT [FK_dbo.Orders_dbo.Customers_CustomerId] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([CustomerId]) ON DELETE CASCADE,
+    CONSTRAINT [FK_dbo.Orders_dbo.Schedules_ScheduleId] FOREIGN KEY ([ScheduleId]) REFERENCES [dbo].[Schedules] ([ScheduleId]) ON DELETE CASCADE
+);
+
