cb27dc350ed933824e37248d048f4f50c5620434
hoang <hoang.mai@smartt.com>
2018-12-05 19:05:01 -0800

    add database project

---
+++ EntityFrameworkCinema/CinemaDB/CinemaDB/Movies.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Movies]
+(
+    [MovieId]      INT           NOT NULL PRIMARY KEY IDENTITY,
+    [Name]         VARCHAR (MAX) NOT NULL,
+    [Description]  VARCHAR (MAX) NOT NULL,
+    [Duration_Minutes]     INT  NOT NULL,
+    [InitialPrice] MONEY         NOT NULL,
+)
